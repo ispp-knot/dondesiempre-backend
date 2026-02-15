022e3a5980154b49ef8b138c4d94c0ddf4fee40e
Emilio-115 <emivazcru@alum.us.es>
2026-02-15 12:34:44 +0100

    feat: :sparkles: Example endpoint and basic SecurityConfig

---
+++ src/main/java/ispp/project/dondesiempre/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package ispp.project.dondesiempre.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/health").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/ispp/project/dondesiempre/controllers/ExampleController.java
@@ -0,0 +1,16 @@
+package ispp.project.dondesiempre.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ExampleController {
+
+    @GetMapping("/health")
+    public String getHealth() {
+        return "Server is up";
+    }
+
+}
