529a51a826604edc2132e59ae8c355f31dc78833
miniluz <javiermelon4fu@gmail.com>
2026-02-20 11:45:35 +0100

    feat: ✨ Error handler controller

---
+++ src/main/java/ispp/project/dondesiempre/controllers/ErrorHandlerController.java
@@ -0,0 +1,16 @@
+package ispp.project.dondesiempre.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class ErrorHandlerController implements ErrorController {
+
+    @GetMapping("/error")
+    public String customError() {
+        throw new ResponseStatusException(HttpStatusCode.valueOf(404));
+    }
+}
