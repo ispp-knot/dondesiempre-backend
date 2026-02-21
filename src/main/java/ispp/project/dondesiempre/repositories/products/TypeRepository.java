da5e29130588f66da4d2f89c273fe471d1197138
IMora04 <ignmorper1@alum.us.es>
2026-02-21 20:54:25 +0100

    feat: add new Product model requirements
    #17

---
+++ src/main/java/ispp/project/dondesiempre/repositories/products/TypeRepository.java
@@ -0,0 +1,6 @@
+package ispp.project.dondesiempre.repositories.products;
+
+import java.lang.reflect.Type;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeRepository extends JpaRepository<Type, Integer> {}
