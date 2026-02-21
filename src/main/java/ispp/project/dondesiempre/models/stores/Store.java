6ec5394c07f19e28abff873711d864da594bca02
IMora04 <ignmorper1@alum.us.es>
2026-02-21 13:57:35 +0100

    feat: add Store model and related entities
    #16

---
+++ src/main/java/ispp/project/dondesiempre/models/stores/Store.java
@@ -0,0 +1,37 @@
+package ispp.project.dondesiempre.models.stores;
+
+import ispp.project.dondesiempre.models.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "stores")
+public class Store extends BaseEntity {
+
+    @Column
+    @NotNull
+    @Size(max = 255)
+    String name;
+
+    @Column
+    @NotNull
+    @Size(max = 255)
+    String location;
+
+    @Column
+    @NotNull
+    @Size(max = 255)
+    String openingHours;
+
+    @Column(columnDefinition = "TEXT")
+    @Size(max = 5000)
+    String aboutUs;
+
+}
