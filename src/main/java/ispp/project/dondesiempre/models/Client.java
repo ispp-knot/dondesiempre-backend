7b15946d344985d4fc4b555d1a3f0a8730721396
IMora04 <ignmorper1@alum.us.es>
2026-02-21 20:27:00 +0100

    feat: add new Client model requirements
    #18

--- src/main/java/ispp/project/dondesiempre/models/Client.java
+++ src/main/java/ispp/project/dondesiempre/models/Client.java
@@ -3,7 +3,9 @@ package ispp.project.dondesiempre.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,12 +17,22 @@ import lombok.Setter;
 public class Client extends BaseEntity {
 
   @Column
-  @NotNull
+  @NotBlank
   @Size(max = 255)
   String name;
 
   @Column
-  @NotNull
+  @NotBlank
   @Size(max = 255)
   String surname;
+
+  @Column @NotBlank @Email String email;
+
+  @Column
+  @Pattern(regexp = "^(\\+\\d{1,3}[- ]?)?\\d{7,15}$")
+  String phone;
+
+  @Column
+  @Size(max = 255)
+  String address;
 }
