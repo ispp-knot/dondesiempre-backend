dd7cdb9163e4a03afa11a767c9280bce68fa4324
IMora04 <ignmorper1@alum.us.es>
2026-02-22 20:58:15 +0100

    refactor: remove Product relation entities. Create ProductVariant entity and repository. Create corresponding migrations
    #17

--- src/main/java/ispp/project/dondesiempre/models/products/ProductVariant.java
+++ src/main/java/ispp/project/dondesiempre/models/products/ProductVariant.java
@@ -1,6 +1,7 @@
 package ispp.project.dondesiempre.models.products;
 
 import ispp.project.dondesiempre.models.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -13,8 +14,8 @@ import org.hibernate.annotations.OnDeleteAction;
 @Entity
 @Getter
 @Setter
-@Table(name = "product_color_relations")
-public class ProductColorRelation extends BaseEntity {
+@Table(name = "product_variants")
+public class ProductVariant extends BaseEntity {
 
   @NotNull
   @ManyToOne(optional = false)
@@ -24,5 +25,12 @@ public class ProductColorRelation extends BaseEntity {
   @NotNull
   @ManyToOne(optional = false)
   @OnDelete(action = OnDeleteAction.CASCADE)
+  private ProductSize size;
+
+  @NotNull
+  @ManyToOne(optional = false)
+  @OnDelete(action = OnDeleteAction.CASCADE)
   private ProductColor color;
+
+  @Column @NotNull Boolean isAvailable;
 }
--- src/main/java/ispp/project/dondesiempre/repositories/products/ProductSizeRelationRepository.java
+++
@@ -1,7 +0,0 @@
-package ispp.project.dondesiempre.repositories.products;
-
-import ispp.project.dondesiempre.models.products.ProductSizeRelation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductSizeRelationRepository
-    extends JpaRepository<ProductSizeRelation, Integer> {}
