4942c2e0c452797e0c3de34810a6269c12edf50d
IMora04 <ignmorper1@alum.us.es>
2026-03-01 19:22:20 +0100

    test: add StoreService tests for follow, unfollow, and getFollows functionalities. Refactor some static test variables
    #48

--- src/test/java/ispp/project/dondesiempre/services/StoreServiceTest.java
+++ src/test/java/ispp/project/dondesiempre/services/StoreServiceTest.java
@@ -2,22 +2,28 @@ package ispp.project.dondesiempre.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import ispp.project.dondesiempre.exceptions.InvalidBoundingBoxException;
 import ispp.project.dondesiempre.exceptions.ResourceNotFoundException;
+import ispp.project.dondesiempre.models.Client;
+import ispp.project.dondesiempre.models.User;
 import ispp.project.dondesiempre.models.stores.Store;
+import ispp.project.dondesiempre.models.stores.StoreFollower;
 import ispp.project.dondesiempre.models.stores.dto.StoreDTO;
+import ispp.project.dondesiempre.repositories.stores.StoreFollowerRepository;
 import ispp.project.dondesiempre.repositories.stores.StoreRepository;
 import ispp.project.dondesiempre.services.stores.StoreService;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,28 +34,53 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class StoreServiceTest {
 
   @Mock private StoreRepository storeRepository;
+  @Mock private StoreFollowerRepository storeFollowerRepository;
+
+  @Mock private UserService userService;
 
   @InjectMocks private StoreService storeService;
 
-  private Store store;
-  private UUID storeId;
+  private static final Client TEST_CLIENT = sampleClient();
+  private static final UUID TEST_STORE_ID = UUID.randomUUID();
+  private static final Store TEST_STORE = sampleStore();
+  private static final StoreFollower TEST_FOLLOWER = sampleFollower();
 
-  @BeforeEach
-  void setUp() {
-    store = new Store();
-    storeId = UUID.randomUUID();
-    store.setId(storeId);
+  private static Store sampleStore() {
+    Store store = new Store();
+    store.setId(TEST_STORE_ID);
     store.setName("Tienda de Prueba");
+
+    return store;
+  }
+
+  private static Client sampleClient() {
+    User user = new User();
+    user.setId(UUID.randomUUID());
+    user.setEmail(UserService.SEED_USER_EMAIL);
+
+    Client client = new Client();
+    client.setId(UUID.randomUUID());
+    client.setUser(user);
+
+    return client;
+  }
+
+  private static StoreFollower sampleFollower() {
+    StoreFollower follower = new StoreFollower();
+    follower.setClient(TEST_CLIENT);
+    follower.setStore(TEST_STORE);
+
+    return follower;
   }
 
   @Test
   void shouldReturnStore_whenFindByIdExists() {
-    when(storeRepository.findById(storeId)).thenReturn(Optional.of(store));
+    when(storeRepository.findById(TEST_STORE_ID)).thenReturn(Optional.of(TEST_STORE));
 
-    Store result = storeService.findById(storeId);
+    Store result = storeService.findById(TEST_STORE_ID);
 
     assertEquals("Tienda de Prueba", result.getName());
-    verify(storeRepository, times(1)).findById(storeId);
+    verify(storeRepository, times(1)).findById(TEST_STORE_ID);
   }
 
   @Test
@@ -67,7 +98,7 @@ public class StoreServiceTest {
     // Dado
     double minLon = -6.0, minLat = 37.0, maxLon = -5.0, maxLat = 38.0;
     when(storeRepository.findStoresInBoundingBox(minLon, minLat, maxLon, maxLat, 500))
-        .thenReturn(List.of(store));
+        .thenReturn(List.of(TEST_STORE));
 
     // Cuando
     List<StoreDTO> result = storeService.findStoresInBoundingBox(minLon, minLat, maxLon, maxLat);
@@ -98,5 +129,74 @@ public class StoreServiceTest {
 
     verify(storeRepository, times(0))
         .findStoresInBoundingBox(anyDouble(), anyDouble(), anyDouble(), anyDouble(), anyInt());
+  }
+
+  @Test
+  void shouldFollowStore() {
+    when(userService.getCurrentClient()).thenReturn(TEST_CLIENT);
+    when(storeRepository.findById(TEST_STORE_ID)).thenReturn(Optional.of(TEST_STORE));
+    when(storeFollowerRepository.save(any())).thenReturn(null);
+
+    StoreFollower follow = storeService.followStore(TEST_STORE_ID);
+
+    assertEquals(follow.getClient().getId(), TEST_CLIENT.getId());
+    assertEquals(follow.getStore().getId(), TEST_STORE_ID);
+    verify(storeFollowerRepository).save(follow);
+  }
+
+  @Test
+  void shouldThrowExceptionIfUnfollowNotFollowedStore() {
+    when(userService.getCurrentClient()).thenReturn(TEST_CLIENT);
+    when(storeFollowerRepository.findByClientIdAndStoreId(TEST_CLIENT.getId(), TEST_STORE_ID))
+        .thenReturn(Optional.empty());
+
+    assertThrows(ResourceNotFoundException.class, () -> storeService.unfollowStore(TEST_STORE_ID));
+    verify(storeFollowerRepository, never()).delete(any());
+  }
+
+  @Test
+  void shouldUnfollowStore() {
+    when(userService.getCurrentClient()).thenReturn(TEST_CLIENT);
+    when(storeFollowerRepository.findByClientIdAndStoreId(TEST_CLIENT.getId(), TEST_STORE_ID))
+        .thenReturn(Optional.of(TEST_FOLLOWER));
+    doNothing().when(storeFollowerRepository).delete(any());
+
+    storeService.unfollowStore(TEST_STORE_ID);
+
+    verify(storeFollowerRepository, times(1)).delete(TEST_FOLLOWER);
+  }
+
+  @Test
+  void shouldGetZeroFollowedStore() {
+    when(userService.getCurrentClient()).thenReturn(TEST_CLIENT);
+    when(storeFollowerRepository.findByClientId(TEST_CLIENT.getId())).thenReturn(List.of());
+
+    List<StoreDTO> stores = storeService.getMyFollowedStores();
+
+    assertEquals(stores.size(), 0);
+  }
+
+  @Test
+  void shouldGetOneFollowedStore() {
+    when(userService.getCurrentClient()).thenReturn(TEST_CLIENT);
+    when(storeFollowerRepository.findByClientId(TEST_CLIENT.getId()))
+        .thenReturn(List.of(TEST_FOLLOWER));
+
+    List<StoreDTO> stores = storeService.getMyFollowedStores();
+
+    assertEquals(stores.size(), 1);
+    assertEquals(stores.get(0).getId(), new StoreDTO(TEST_STORE).getId());
+  }
+
+  @Test
+  void shouldGetThreeFollowedStore() {
+    when(userService.getCurrentClient()).thenReturn(TEST_CLIENT);
+    when(storeFollowerRepository.findByClientId(TEST_CLIENT.getId()))
+        .thenReturn(List.of(TEST_FOLLOWER, TEST_FOLLOWER, TEST_FOLLOWER));
+
+    List<StoreDTO> stores = storeService.getMyFollowedStores();
+
+    assertEquals(stores.size(), 3);
+    assertEquals(stores.get(0).getId(), new StoreDTO(TEST_STORE).getId());
   }
 }
