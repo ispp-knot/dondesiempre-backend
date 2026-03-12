49a1903104a54bf3a3c6771f8b088909a4349d41
Emilio-115 <emiliovc2016@gmail.com>
2026-03-12 17:29:06 +0100

    refactor: :recycle: add missing validation check in product creation

--- src/main/java/ispp/project/dondesiempre/modules/products/dtos/ProductCreationDTO.java
+++ src/main/java/ispp/project/dondesiempre/modules/products/dtos/ProductCreationDTO.java
@@ -1,5 +1,8 @@
 package ispp.project.dondesiempre.modules.products.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,10 +13,18 @@ import lombok.Setter;
 @NoArgsConstructor
 public class ProductCreationDTO {
 
+  @NotNull
+  @Size(max = 255)
   private String name;
+
+  @NotNull
+  @Min(0)
   private Integer priceInCents;
+
   private Integer discountedPriceInCents;
+
+  @Size(max = 5000)
   private String description;
 
-  private UUID typeId;
+  @NotNull private UUID typeId;
 }
