a06beacbd4a0aa0bacfdcf72349b4eb265d01e53
Emilio-115 <emiliovc2016@gmail.com>
2026-03-12 12:18:17 +0100

    refactor: :recycle: add validation reference in discount modification dto

--- src/main/java/ispp/project/dondesiempre/modules/products/dtos/DiscountModificationDTO.java
+++ src/main/java/ispp/project/dondesiempre/modules/products/dtos/DiscountModificationDTO.java
@@ -1,5 +1,6 @@
 package ispp.project.dondesiempre.modules.products.dtos;
 
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,5 +10,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class DiscountModificationDTO {
 
+  @Min(0)
   private Integer discountedPriceInCents;
 }
