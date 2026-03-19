30951f5ad3a13cc52f2254f77221e667bf9648f5
Aarón Mayoral <aaronmayoralansias@gmail.com>
2026-03-19 18:05:16 +0100

    fix: add validations to ProductUpdatDTO

--- src/main/java/ispp/project/dondesiempre/modules/products/dtos/ProductUpdateDTO.java
+++ src/main/java/ispp/project/dondesiempre/modules/products/dtos/ProductUpdateDTO.java
@@ -1,12 +1,24 @@
 package ispp.project.dondesiempre.modules.products.dtos;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import java.util.UUID;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class ProductUpdateDTO {
+
+  @Size(max = 255)
   private String name;
+
   private String description;
+
+  @Min(0)
   private Integer priceInCents;
+
   private UUID productTypeId;
 }
