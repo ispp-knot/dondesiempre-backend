afd042030962e7497610e81ae28f702ddfd5d84a
Estrella Ángel <estrella.angel.postigo@gmail.com>
2026-03-21 21:47:51 +0100

    fix: update store service

--- src/main/java/ispp/project/dondesiempre/modules/stores/services/StoreService.java
+++ src/main/java/ispp/project/dondesiempre/modules/stores/services/StoreService.java
@@ -130,8 +130,8 @@ public class StoreService {
     if (dto.getEmail() != null) storeToUpdate.setEmail(dto.getEmail());
     if (dto.getAddress() != null) storeToUpdate.setAddress(dto.getAddress());
     if (dto.getOpeningHours() != null) storeToUpdate.setOpeningHours(dto.getOpeningHours());
-    if (dto.getPhone() != null) storeToUpdate.setPhone(dto.getPhone());
-    if (dto.getAboutUs() != null) storeToUpdate.setAboutUs(dto.getAboutUs());
+    storeToUpdate.setPhone(dto.getPhone());
+    storeToUpdate.setAboutUs(dto.getAboutUs());
 
     return applicationContext
         .getBean(StoreService.class)
