0bd3af6d816a19f03f0fea7fb72621c2e4770710
Pepe Coral <coritochico@gmail.com>
2026-03-23 18:27:01 +0100

    refactor: :truck: moved govantes' images

--- src/main/java/ispp/project/dondesiempre/seed/DataSeeder.java
+++ src/main/java/ispp/project/dondesiempre/seed/DataSeeder.java
@@ -594,7 +594,7 @@ public class DataSeeder implements CommandLineRunner {
         "Lun,Mar,Jue,Vie: 9:30-14:00. Mie: 9:30-14:00, 5:30-8:30. Sab 10:00-13:30",
         "#000045",
         "#844652",
-        "escaparate_mar_govantes.jpg",
+        "govantes/escaparate_mar_govantes.jpg",
         "👗Tienda de ropa especializada en #moda mujer en Dos Hermanas (Sevilla).\n"
             + //
             "Ropa casual y de vestir.\n"
@@ -615,7 +615,7 @@ public class DataSeeder implements CommandLineRunner {
         "Americana marina, perfecta para looks formales o de oficina.",
         productTypes.get("Chaqueta"),
         marGovantes,
-        "outfit_9_americana_marina_45.99.jpg");
+        "govantes/outfit_9_americana_marina_45.99.jpg");
     createVariant(mg_americanaMarina, productSizes.get("M"), productColors.get("Azul"), true);
 
     Product mg_pantalonesMari = createProduct(
@@ -625,7 +625,7 @@ public class DataSeeder implements CommandLineRunner {
         "Pantalones marinos, elegantes y apropiados para un look formal.",
         productTypes.get("Pantalón"),
         marGovantes,
-        "outfit_9_pantalones_marinos_39.99.jpg");
+        "govantes/outfit_9_pantalones_marinos_39.99.jpg");
     createVariant(mg_pantalonesMari, productSizes.get("M"), productColors.get("Azul"), true);
 
     Product mg_camisaFloral = createProduct(
@@ -635,7 +635,7 @@ public class DataSeeder implements CommandLineRunner {
         "Camisa floral, una opción fresca y alegre para el buen tiempo.",
         productTypes.get("Camiseta"),
         marGovantes,
-        "outfit_10_camisa_floral_23.99.jpg");
+        "govantes/outfit_10_camisa_floral_23.99.jpg");
     createVariant(mg_camisaFloral, productSizes.get("M"), productColors.get("Rosa"), true);
 
     Product mg_pantalonesVerdes = createProduct(
@@ -645,7 +645,7 @@ public class DataSeeder implements CommandLineRunner {
         "Pantalones verdes, ideales para un look casual de primavera.",
         productTypes.get("Pantalón"),
         marGovantes,
-        "outfit_10_pantalones_verdes_34.99.jpg");
+        "govantes/outfit_10_pantalones_verdes_34.99.jpg");
     createVariant(mg_pantalonesVerdes, productSizes.get("M"), productColors.get("Verde"), true);
 
     Product mg_sueterVerde = createProduct(
@@ -655,15 +655,16 @@ public class DataSeeder implements CommandLineRunner {
         "Suéter verde, apropiado para el entretiempo y los días frescos.",
         productTypes.get("Chaqueta"),
         marGovantes,
-        "outfit_10_suéter_verde_45_99.jpg");
+        "govantes/outfit_10_suéter_verde_45_99.jpg");
     createVariant(mg_sueterVerde, productSizes.get("M"), productColors.get("Verde"), true);
 
-    Outfit mg_outfit1 = createOutfit("Negocio Oceánico", 0, null, marGovantes, "outfit_9_negocio_oceánico.jpg");
+    Outfit mg_outfit1 = createOutfit("Negocio Oceánico", 0, null, marGovantes,
+        "govantes/outfit_9_negocio_oceánico.jpg");
     createOutfitTagRelation(mg_outfit1, outfitTags.get("Formal"));
     createOutfitProduct(mg_outfit1, mg_americanaMarina, 0);
     createOutfitProduct(mg_outfit1, mg_pantalonesMari, 1);
 
-    Outfit mg_outfit2 = createOutfit("Río Interior", 1, null, marGovantes, "outfit_10_río_interior.jpg");
+    Outfit mg_outfit2 = createOutfit("Río Interior", 1, null, marGovantes, "govantes/outfit_10_río_interior.jpg");
     createOutfitTagRelation(mg_outfit2, outfitTags.get("Casual"));
     createOutfitProduct(mg_outfit2, mg_camisaFloral, 0);
     createOutfitProduct(mg_outfit2, mg_pantalonesVerdes, 1);
