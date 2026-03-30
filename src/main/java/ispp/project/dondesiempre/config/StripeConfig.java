fdcd1176e3ef02da49a8a21aa2393b2daf0d36a1
Emilio-115 <emiliovc2016@gmail.com>
2026-03-30 17:10:17 +0200

    refactor: :bug: add conditional if not running in web app context

--- src/main/java/ispp/project/dondesiempre/config/StripeConfig.java
+++ src/main/java/ispp/project/dondesiempre/config/StripeConfig.java
@@ -3,9 +3,11 @@ package ispp.project.dondesiempre.config;
 import com.stripe.Stripe;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnWebApplication
 public class StripeConfig {
 
   @Value("${stripe.key.secret}")
